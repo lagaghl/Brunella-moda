d8f385b1dcbecb0745da0adbd44384014a2f766a
lagaghl <jhonatan.nieva323@gmail.com>
2023-07-30 02:58:28 -0500

    Se termina de arreglar el nav responsive

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const next = document.querySelector('.next');
 const previous = document.querySelector('.previous');
 const carrousel = document.querySelector('.carrousel');
+const hamburger = document.querySelector('.nav__hamburger-container');
+const navUl = document.querySelector('.nav__ul-phone');
 let CarrouselIndex = 0;
 
 next.addEventListener('click',()=>{
@@ -15,4 +17,16 @@ previous.addEventListener('click', ()=>{
     carrousel.style.transform = `translateX(calc((-100%/3)*${CarrouselIndex-1}))`;
     CarrouselIndex--;
     console.log(CarrouselIndex);
+})
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('open');
+    if (hamburger.classList.contains('open')) {
+        navUl.style.display = 'flex';
+        navUl.style.animation = 'appear 0.3s forwards'
+    }else{
+        navUl.style.animation = 'desappear 0.3s forwards'
+        setTimeout(()=>{
+            navUl.style.display = 'none';
+        },300)
+    }
 })
